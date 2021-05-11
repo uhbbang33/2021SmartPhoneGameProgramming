48d82251c18eb22c1d057ce6d086e1663b57c4f6
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2021-05-11 16:51:42 +0900

    init Project

---
+++ SmartPhoneGameprogrammingTermProject/SmartPhoneGameprogrammingTermProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartPhoneGameprogrammingTermProject
+//
+//  Created by  kpugame on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
